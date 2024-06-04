9ab57f71615ad006ab0d55ed528d675d1bdd9996
Farshad <farshad.1991@gmail.com>
2024-06-04 11:26:24 -0600

    [ADDED](MINOR) Image presentation updated

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+// Function to open full-size image
+function openFullSize(src) {
+  var overlay = document.getElementById("fullSizeImageOverlay");
+  var fullSizeImage = document.getElementById("fullSizeImage");
+  fullSizeImage.src = src;
+  overlay.style.display = "flex";
+}
+
+// Function to close full-size image
+function closeFullSize() {
+  var overlay = document.getElementById("fullSizeImageOverlay");
+  overlay.style.display = "none";
+}
